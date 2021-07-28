9e57c1df038d659e3ba93aeed771b288a3c9702c
DavidKosalRevature <david.kosal@revature.net>
2021-07-28 11:34:35 -0500

    Day 3 demo

---
+++ Day 3 Demo/src/com/company/Demo10.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Demo10 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append("World");
+        System.out.println(sb); //HelloWorld
+    }
+
+}
