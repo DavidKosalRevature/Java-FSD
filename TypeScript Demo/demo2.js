154f6fd56d8618813a90194dae15d7b21a2e0d83
DavidKosalRevature <david.kosal@revature.net>
2021-08-25 10:18:34 -0500

    TypeScript Demo

---
+++ TypeScript Demo/demo2.js
@@ -0,0 +1,11 @@
+//var - to declare variable
+//Let
+//const
+//var vs let
+var a = 100;
+console.log(a); // 100
+{
+    var a = 200;
+    console.log(a); // 200
+}
+console.log(a); // 200
---
+++ TypeScript Demo/demo2.ts
@@ -0,0 +1,15 @@
+//var - to declare variable
+
+//Let
+//const
+
+//var vs let
+
+var a = 100;
+console.log(a); // 100
+{
+    var a = 200;
+    console.log(a) // 200
+}
+
+console.log(a) // 200
